d86bb448a9a5a4f9e170048085b2247d2c11ddc2
Gabriele Martina <contattagmstyle@gmail.com>
2023-09-09 16:17:29 +0200

    Update dashboard_view.dart

--- lib/ui/views/dashboard/dashboard_view.dart
+++ lib/ui/views/dashboard/dashboard_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_tube/blocs/home/home_bloc.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({Key? key}) : super(key: key);
@@ -9,7 +11,11 @@ class DashboardView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Dashboard'),
       ),
-      body: const Center(child: Text('Dashboard')),
+      body: BlocBuilder<HomeBloc, HomeState>(
+        builder: (context, state) {
+          return Center(child: Text('Dashboard'));
+        },
+      ),
     );
   }
 }
