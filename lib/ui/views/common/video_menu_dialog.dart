fd3792fb12dfce165bb082c488b35e635583dfcb
Gabriele Martina <gabriele.martina@icsone.it>
2024-01-16 12:20:11 +0100

    video menu dialog

---
+++ lib/ui/views/common/video_menu_dialog.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:my_tube/blocs/home/favorites_tab/favorites_bloc.dart';
+import 'package:my_tube/blocs/home/mini_player_cubit/mini_player_cubit.dart';
+import 'package:my_tube/models/resource_mt.dart';
+
+class VideoMenuDialog extends StatelessWidget {
+  const VideoMenuDialog({super.key, required this.video, required this.child});
+  final ResourceMT video;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final favoritesBloc = context.read<FavoritesBloc>();
+
+    final MiniPlayerCubit miniPlayerCubit =
+        BlocProvider.of<MiniPlayerCubit>(context);
+    return GestureDetector(
+      onLongPress: () {
+        // show option dialog
+        showDialog(
+            context: context,
+            builder: (_) {
+              return AlertDialog(
+                //title: const Text('Options'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // show the option to remove the video from the favorites if it is in the favorites
+                    if (favoritesBloc.favoritesRepository.videoIds
+                        .contains(video.id))
+                      ListTile(
+                        leading: const Icon(Icons.remove),
+                        title: const Text('Remove from favorites'),
+                        onTap: () {
+                          favoritesBloc.add(RemoveFromFavorites(video.id!));
+                          context.pop();
+                        },
+                      ),
+
+                    // show the option to add the video to the favorites if it is not in the favorites
+                    if (!favoritesBloc.favoritesRepository.videoIds
+                        .contains(video.id))
+                      ListTile(
+                        leading: const Icon(Icons.favorite),
+                        title: const Text('Add to favorites'),
+                        onTap: () {
+                          favoritesBloc.add(AddToFavorites(video));
+                          context.pop();
+                        },
+                      ),
+
+                    // show the option to add the video to the queue if it is not in the queue
+                    if (!miniPlayerCubit.mtPlayerHandler.queue.value
+                        .map((e) => e.id)
+                        .contains(video.id))
+                      ListTile(
+                        leading: const Icon(Icons.playlist_add),
+                        title: const Text('Add to queue'),
+                        onTap: () {
+                          miniPlayerCubit.addToQueue(video.id!);
+                          context.pop();
+                        },
+                      ),
+
+                    // show the option to remove the video from the queue if it is in the queue
+                    if (miniPlayerCubit.mtPlayerHandler.queue.value
+                        .map((e) => e.id)
+                        .contains(video.id))
+                      ListTile(
+                        leading: const Icon(Icons.remove),
+                        title: const Text('Remove from queue'),
+                        onTap: () {
+                          miniPlayerCubit.removeFromQueue(video);
+                          context.pop();
+                        },
+                      ),
+                  ],
+                ),
+              );
+            });
+      },
+      child: child,
+    );
+  }
+}
--- lib/ui/views/common/video_tile.dart
+++ lib/ui/views/common/video_tile.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-import 'package:my_tube/blocs/home/favorites_tab/favorites_bloc.dart';
+
 import 'package:my_tube/blocs/home/mini_player_cubit/mini_player_cubit.dart';
 import 'package:my_tube/models/resource_mt.dart';
 import 'package:my_tube/ui/views/common/audio_spectrum_icon.dart';
+import 'package:my_tube/ui/views/common/video_menu_dialog.dart';
 
 class VideoTile extends StatelessWidget {
   const VideoTile({super.key, required this.video});
@@ -13,76 +13,10 @@ class VideoTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final favoritesBloc = context.read<FavoritesBloc>();
-
     final MiniPlayerCubit miniPlayerCubit =
         BlocProvider.of<MiniPlayerCubit>(context);
-
-    return GestureDetector(
-      onLongPress: () {
-        // show option dialog
-        showDialog(
-            context: context,
-            builder: (_) {
-              return AlertDialog(
-                //title: const Text('Options'),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // show the option to remove the video from the favorites if it is in the favorites
-                    if (favoritesBloc.favoritesRepository.videoIds
-                        .contains(video.id))
-                      ListTile(
-                        leading: const Icon(Icons.remove),
-                        title: const Text('Remove from favorites'),
-                        onTap: () {
-                          favoritesBloc.add(RemoveFromFavorites(video.id!));
-                          context.pop();
-                        },
-                      ),
-
-                    // show the option to add the video to the favorites if it is not in the favorites
-                    if (!favoritesBloc.favoritesRepository.videoIds
-                        .contains(video.id))
-                      ListTile(
-                        leading: const Icon(Icons.favorite),
-                        title: const Text('Add to favorites'),
-                        onTap: () {
-                          favoritesBloc.add(AddToFavorites(video));
-                          context.pop();
-                        },
-                      ),
-
-                    // show the option to add the video to the queue if it is not in the queue
-                    if (!miniPlayerCubit.mtPlayerHandler.queue.value
-                        .map((e) => e.id)
-                        .contains(video.id))
-                      ListTile(
-                        leading: const Icon(Icons.playlist_add),
-                        title: const Text('Add to queue'),
-                        onTap: () {
-                          miniPlayerCubit.addToQueue(video.id!);
-                          context.pop();
-                        },
-                      ),
-
-                    // show the option to remove the video from the queue if it is in the queue
-                    if (miniPlayerCubit.mtPlayerHandler.queue.value
-                        .map((e) => e.id)
-                        .contains(video.id))
-                      ListTile(
-                        leading: const Icon(Icons.remove),
-                        title: const Text('Remove from queue'),
-                        onTap: () {
-                          miniPlayerCubit.removeFromQueue(video);
-                          context.pop();
-                        },
-                      ),
-                  ],
-                ),
-              );
-            });
-      },
+    return VideoMenuDialog(
+      video: video,
       child: Container(
         height: MediaQuery.of(context).size.height * 0.1,
         margin: const EdgeInsets.only(bottom: 16),
@@ -200,14 +134,14 @@ class VideoTile extends StatelessWidget {
                 itemBuilder: (context) {
                   return [
                     // show the option to remove the video from the queue if it is in the queue
-
+      
                     PopupMenuItem(
                         value: 'remove',
                         child: const Text('Remove from favorites'),
                         onTap: () async {
                           
                         }),
-
+      
                     // show the option to add the video to the queue if it is not in the queue
                   ];
                 },
