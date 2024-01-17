d8484f6150c9173227bbcac334362eb841009391
Gabriele Martina <gabriele.martina@icsone.it>
2024-01-17 11:00:46 +0100

    WIP queue, ok media item tile e draggable sheet

---
+++ lib/ui/views/video_view/widget/mediaitem_tile.dart
@@ -0,0 +1,130 @@
+import 'package:audio_service/audio_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:my_tube/blocs/home/mini_player_cubit/mini_player_cubit.dart';
+import 'package:my_tube/ui/views/common/audio_spectrum_icon.dart';
+
+class MediaitemTile extends StatelessWidget {
+  const MediaitemTile({super.key, required this.video});
+
+  final MediaItem video;
+
+  @override
+  Widget build(BuildContext context) {
+    final MiniPlayerCubit miniPlayerCubit =
+        BlocProvider.of<MiniPlayerCubit>(context);
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1,
+      margin: const EdgeInsets.only(bottom: 16),
+      padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.03),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  video.artUri != null
+                      ? Image.network(
+                          video.artUri!.toString(),
+                          height: MediaQuery.of(context).size.height * 0.09,
+                          width: MediaQuery.of(context).size.width * 0.2,
+                          fit: BoxFit.cover,
+                        )
+                      : ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: const SizedBox(
+                            child: FlutterLogo(),
+                          ),
+                        ),
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.transparent,
+                          Colors.black.withOpacity(0.4),
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 4,
+                    bottom: 4,
+                    left: 4,
+                    right: 0,
+                    child: Column(
+                      children: [
+                        const Spacer(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            // show an animated that the video is playing
+                            StreamBuilder(
+                                stream:
+                                    miniPlayerCubit.mtPlayerHandler.mediaItem,
+                                builder: (context, snapshot) {
+                                  if (snapshot.hasData) {
+                                    final currentVideoId = snapshot.data!.id;
+                                    if (currentVideoId == video.id) {
+                                      return StreamBuilder(
+                                          stream: miniPlayerCubit
+                                              .mtPlayerHandler.playbackState
+                                              .map((playbackState) =>
+                                                  playbackState.playing)
+                                              .distinct(),
+                                          builder: (context, snapshot) {
+                                            if (snapshot.hasData) {
+                                              final isPlaying =
+                                                  snapshot.data ?? false;
+                                              if (isPlaying) {
+                                                return const AudioSpectrumIcon();
+                                              }
+                                            }
+                                            return const SizedBox();
+                                          });
+                                    }
+                                  }
+                                  return const SizedBox();
+                                }),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 16,
+          ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  video.title,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge
+                      ?.copyWith(fontWeight: FontWeight.bold),
+                  maxLines: 2,
+                ),
+                Text(video.album ?? '',
+                    style: Theme.of(context).textTheme.bodySmall),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/views/video_view/widget/queue_draggable_sheet.dart
+++ lib/ui/views/video_view/widget/queue_draggable_sheet.dart
@@ -1,71 +1,81 @@
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_tube/blocs/home/mini_player_cubit/mini_player_cubit.dart';
 import 'package:my_tube/services/mt_player_handler.dart';
+import 'package:my_tube/ui/views/video_view/widget/mediaitem_tile.dart';
 
 class QueueDraggableSheet extends StatelessWidget {
-  const QueueDraggableSheet({super.key});
+  QueueDraggableSheet({super.key});
 
+  final controller = DraggableScrollableController();
+  final minChildSize = 0.05;
+  final maxChildSize = 1.0;
+  final snapSizes = const [
+    0.5,
+  ];
   @override
   Widget build(BuildContext context) {
-    final mtPlayerHandler = context.read<MtPlayerHandler>();
+    final miniPlayerCubit = context.read<MiniPlayerCubit>();
 
     return DraggableScrollableSheet(
-        initialChildSize: 0.05,
-        minChildSize: 0.05,
+        controller: controller,
+        initialChildSize: minChildSize,
+        minChildSize: minChildSize,
+        maxChildSize: maxChildSize,
         snap: true,
-        snapSizes: const [
-          0.5,
-        ],
+        snapSizes: snapSizes,
         builder: (context, scrollController) {
           return Container(
             decoration: BoxDecoration(
                 color: Theme.of(context).colorScheme.surface,
                 borderRadius: const BorderRadius.only(
                     topLeft: Radius.circular(8), topRight: Radius.circular(8))),
-            child: Column(
+            child: Stack(
               children: [
-                SingleChildScrollView(
-                  controller: scrollController,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 4),
-                    child: Column(children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 4,
-                            width: 40,
-                            decoration: BoxDecoration(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .onSurface
-                                    .withOpacity(0.5),
-                                borderRadius: BorderRadius.circular(2)),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text('Queue',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .titleLarge
-                                  ?.copyWith(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onSurface)),
-                        ],
-                      ),
-                    ]),
+                InkWell(
+                  onTap: _expandCollapse,
+                  child: SingleChildScrollView(
+                    controller: scrollController,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 4),
+                      child: Column(children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 4,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onSurface
+                                      .withOpacity(0.5),
+                                  borderRadius: BorderRadius.circular(2)),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        const Text(
+                          'QUEUE',
+                        ),
+                      ]),
+                    ),
                   ),
                 ),
-                Expanded(
+                const SizedBox(
+                  height: 8,
+                ),
+                Positioned(
+                  top: 40,
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
                   child: StreamBuilder<List<MediaItem>>(
-                      stream: mtPlayerHandler.queue,
+                      stream: miniPlayerCubit.mtPlayerHandler.queue,
                       builder: (context, snapshot) {
                         if (snapshot.hasData) {
                           final queue = snapshot.data!;
@@ -73,18 +83,12 @@ class QueueDraggableSheet extends StatelessWidget {
                               itemCount: queue.length,
                               itemBuilder: (context, index) {
                                 final mediaItem = queue[index];
-                                return ListTile(
-                                  leading: ClipRRect(
-                                    borderRadius: BorderRadius.circular(8),
-                                    child: Image.network(
-                                        mediaItem.artUri.toString()),
-                                  ),
-                                  title: Text(mediaItem.title ?? ''),
-                                  subtitle: Text(mediaItem.album ?? ''),
-                                  onTap: () {
-                                    // TODO: Implement onTap
-                                  },
-                                );
+                                return GestureDetector(
+                                    onTap: () {
+                                      miniPlayerCubit
+                                          .startPlaying(mediaItem.id);
+                                    },
+                                    child: MediaitemTile(video: mediaItem));
                               });
                         } else {
                           return const SizedBox();
@@ -95,5 +99,22 @@ class QueueDraggableSheet extends StatelessWidget {
             ),
           );
         });
+  }
+
+  void _expandCollapse() {
+    // se il draggable sheet è aperto lo chiudo altrimenti lo apro
+    if (controller.size == minChildSize) {
+      controller.animateTo(
+        maxChildSize,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      controller.animateTo(
+        minChildSize,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+      );
+    }
   }
 }
