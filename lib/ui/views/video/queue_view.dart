67550d9a20e6d5b0892ec0be374c67200122553d
contattagmstyle <contattagmstyle@gmail.com>
2026-03-06 11:56:24 +0000

    fix mini player in queue_view

--- lib/ui/views/video/queue_view.dart
+++ lib/ui/views/video/queue_view.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:my_tube/blocs/persistent_ui/persistent_ui_cubit.dart';
 import 'package:my_tube/ui/views/video/widget/queue_draggable_sheet/clear_queue_button.dart';
 import 'package:my_tube/ui/views/video/widget/queue_draggable_sheet/media_item_list.dart';
 
-class QueueView extends StatelessWidget {
+class QueueView extends StatefulWidget {
   const QueueView({super.key});
+
+  @override
+  State<QueueView> createState() => _QueueViewState();
+}
+
+class _QueueViewState extends State<QueueView> {
+
+  late final PersistentUiCubit persistentUiCubit;
+
+  @override
+  void initState() {
+    super.initState();
+    persistentUiCubit = context.read<PersistentUiCubit>();
+
+  }
+  @override
+  void dispose() {
+
+    if (persistentUiCubit.state.isPlayerVisible) {
+      persistentUiCubit.setPlayerVisibility(true);
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
